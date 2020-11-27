298dd27bfa6c539137f8de070244e97738730428
Ivan Shamatov <status.enable@gmail.com>
2020-11-27 09:20:10 +0300

    hotfix/return name into schema

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -5,6 +5,7 @@ module Types
     field :id, ID, null: false
     field :email, String, null: false
     field :nickname, String, null: false
+    field :name, String, null: false
     field :avatar, Types::AvatarType, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :avatar, :nickname, :first_name, :last_name
+  attributes :id, :email, :avatar, :nickname, :first_name, :last_name, :name
+
+  def name
+    "#{object.first_name} #{object.last_name}"
+  end
 end
