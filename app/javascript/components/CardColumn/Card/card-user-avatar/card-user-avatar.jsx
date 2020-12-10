eda0bc2107b317d6c574c6039ea0f42d87802b3e
Sooonne <maria.providukhina@cybergizer.com>
2020-12-10 22:34:47 +0300

    improve user avatar component

--- app/javascript/components/CardColumn/Card/card-user-avatar/card-user-avatar.jsx
+++ app/javascript/components/CardColumn/Card/card-user-avatar/card-user-avatar.jsx
@@ -2,19 +2,15 @@ import React from 'react';
 import {getUserInitials} from '../../../../utils/helpers';
 
 const CardUserAvatar = ({avatar, firstName, lastName}) => {
-  const renderAvatar = (userAvatar, userName, userSurname) => {
-    if (userAvatar) {
-      return <img src={userAvatar} className="avatar" />;
-    }
+  if (avatar) {
+    return <img src={avatar} className="avatar" />;
+  }
 
-    return (
-      <div className="avatar avatar--text">
-        {getUserInitials(userName, userSurname)}
-      </div>
-    );
-  };
-
-  return renderAvatar(avatar, firstName, lastName);
+  return (
+    <div className="avatar avatar--text">
+      {getUserInitials(firstName, lastName)}
+    </div>
+  );
 };
 
 export default CardUserAvatar;
