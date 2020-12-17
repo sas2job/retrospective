41ec045d90c32a8d273309e7815940d3b1c6709b
Maria Frei <itimasha@gmail.com>
2020-12-17 12:11:03 +0300

    add focus and hotkeys (#199)
    * add focus and hotkeys
    
    * fix

--- app/javascript/components/CardColumn/Card/CommentsDropdown/index.js
+++ app/javascript/components/CardColumn/Card/CommentsDropdown/index.js
@@ -1,4 +1,4 @@
-import React, {useRef, useState, useContext} from 'react';
+import React, {useRef, useState, useContext, useEffect} from 'react';
 import Picker from 'emoji-picker-react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faSmile} from '@fortawesome/free-regular-svg-icons';
@@ -7,13 +7,18 @@ import Comment from './Comment';
 import {useMutation} from '@apollo/react-hooks';
 import {addCommentMutation} from './operations.gql';
 
-const CommentsDropdown = ({id, comments}) => {
+const CommentsDropdown = ({id, comments, onClickClosed}) => {
   const controlElement = useRef(null);
+  const textInput = useRef();
   const [showEmojiPicker, setShowEmojiPicker] = useState(false);
   const [isError, setIsError] = useState(false);
   const user = useContext(UserContext);
   const [newComment, setNewComment] = useState('');
   const [addComment] = useMutation(addCommentMutation);
+
+  useEffect(() => {
+    textInput.current.focus();
+  }, []);
 
   const handleErrorSubmit = () => {
     setNewComment('');
@@ -25,12 +30,12 @@ const CommentsDropdown = ({id, comments}) => {
     setIsError(false);
   };
 
-  const handleSubmit = (commentContent) => {
+  const handleSubmit = () => {
     controlElement.current.disabled = true;
     addComment({
       variables: {
         cardId: id,
-        content: commentContent
+        content: newComment
       }
     }).then(({data}) => {
       if (data.addComment.comment) {
@@ -52,16 +57,32 @@ const CommentsDropdown = ({id, comments}) => {
     setNewComment((comment) => `${comment}${emoji.emoji}`);
   };
 
+  const handleKeyPress = (evt) => {
+    if (navigator.platform.includes('Mac')) {
+      if (evt.key === 'Enter' && evt.metaKey) {
+        handleSubmit(evt);
+      }
+    } else if (evt.key === 'Enter' && evt.ctrlKey) {
+      handleSubmit(evt);
+    }
+
+    if (evt.key === 'Escape') {
+      onClickClosed();
+    }
+  };
+
   return (
     <div className="column comments-column">
       <div className="dropdown-comments-menu" role="menu">
         <div className="dropdown-comments-content">
           <div className="textarea-container dropdown-item">
             <textarea
+              ref={textInput}
               className="textarea"
               value={newComment}
               style={isError ? {outline: 'solid 1px red'} : {}}
               onChange={(evt) => setNewComment(evt.target.value)}
+              onKeyDown={handleKeyPress}
             />
             <div className="edit-panel-wrapper">
               <a className="has-text-info" onClick={handleSmileClick}>
