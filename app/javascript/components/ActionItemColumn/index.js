857d90a3a4aa3548e7eba73eadb12f1b9fec8ecc
Sooonne <maria.providukhina@cybergizer.com>
2020-12-17 12:14:25 +0300

    first add items to frontend

--- app/javascript/components/ActionItemColumn/index.js
+++ app/javascript/components/ActionItemColumn/index.js
@@ -3,6 +3,7 @@ import {useSubscription} from '@apollo/react-hooks';
 import NewActionItemBody from '../new-action-item-body/new-action-item-body';
 import ActionItem from '../ActionItem';
 import BoardSlugContext from '../../utils/board_slug_context';
+// Import UserContext from '../../utils/user-context';
 import {
   actionItemAddedSubscription,
   actionItemMovedSubscription,
@@ -28,13 +29,21 @@ const ActionItemColumn = ({users, initItems}) => {
     variables: {boardSlug}
   });
 
+  // Let firstObj = myChartArray.find(item => item.Key === 'FirstObj');
+
+  const checkIsItemAddedById = (array, addedItem) => {
+    array.find((it) => it.id.toString() === addedItem.id).toString();
+  };
+
   useSubscription(actionItemAddedSubscription, {
     skip,
     onSubscriptionData: (options) => {
       const {data} = options.subscriptionData;
       const {actionItemAdded} = data;
       if (actionItemAdded) {
-        setItems((oldItems) => [actionItemAdded, ...oldItems]);
+        if (!checkIsItemAddedById(items, actionItemAdded)) {
+          setItems((oldItems) => [actionItemAdded, ...oldItems]);
+        }
       }
     },
     variables: {boardSlug}
@@ -89,7 +98,18 @@ const ActionItemColumn = ({users, initItems}) => {
 
   return (
     <>
-      <NewActionItemBody users={users} />
+      <NewActionItemBody
+        users={users}
+        onItemAdded={(itemAdded) => {
+          setItems((items) => [itemAdded, ...items]);
+        }}
+        onGetNewItemId={(itemMockId, itemId) => {
+          setItems((items) => {
+            items[items.findIndex((it) => it.id === itemMockId)].id = itemId;
+            return items;
+          });
+        }}
+      />
       {items.map((item) => {
         return <ActionItem key={item.id} {...item} users={users} />;
       })}
