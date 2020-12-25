61bacea4da516cc2ac6b3a1f9e28bde98702f799
Ivan Shamatov <status.enable@gmail.com>
2020-12-25 17:02:34 +0300

    migration fix

--- db/migrate/20201225131656_populate_author_id_to_action_items.rb
+++ db/migrate/20201225131656_populate_author_id_to_action_items.rb
@@ -4,7 +4,12 @@ class PopulateAuthorIdToActionItems < ActiveRecord::Migration[6.0]
   def change
     Board.find_each do |board|
       creator = board.memberships.where(role: 'creator').first
-      board.action_items.update_all(author_id: creator.user_id)
+      if creator
+        board.action_items.update_all(author_id: creator.user_id)
+      else
+        c = board.memberships.first.update(role: 'creator')
+        board.action_items.update_all(author_id: c.user_id)
+      end
     end
   end
 end
