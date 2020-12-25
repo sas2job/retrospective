d72396ca375ca36441c8a01e0d3ad20ca28f3052
Vladimir Kattsov <kattsoff@gmail.com>
2020-12-25 18:34:56 +0300

    Try to set paths option for less loader

--- config/webpack/loaders/less.js
+++ config/webpack/loaders/less.js
@@ -1,9 +1,12 @@
+const path = require('path');
 const getStyleRule = require('@rails/webpacker/package/utils/get_style_rule');
+
 module.exports = getStyleRule(/\.less$/i, true, [
   {
     loader: 'less-loader',
     options: {
       lessOptions: {
+        paths: [path.resolve(__dirname, 'node_modules')],
         strictMath: true
       }
     }
