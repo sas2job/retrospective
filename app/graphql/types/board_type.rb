8a6f24f83ee5c6c645d0cdecf80033803faa0acc
ekifel <kifel.ernest@gmail.com>
2020-12-28 20:43:41 +0500

    adding info about current user and previous action items

--- app/graphql/types/board_type.rb
+++ app/graphql/types/board_type.rb
@@ -7,8 +7,18 @@ module Types
     field :slug, String, null: false
     field :cards, [Types::CardType], null: false
     field :memberships, [Types::MembershipType], null: false
+    field :current_membership, Types::MembershipType, null: false
     field :action_items, [Types::ActionItemType], null: false
+    field :previous_action_items, [Types::ActionItemType], null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+    def previous_action_items
+      object.previous_board.action_items if object.previous_board&.action_items&.any?
+    end
+
+    def current_membership
+      object.memberships.find_by(user: context[:current_user])
+    end
   end
 end
