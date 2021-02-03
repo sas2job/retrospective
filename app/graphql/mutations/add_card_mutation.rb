39e686893c710285f611cad3e21767e328370bbb
Ekaterina Egorova <admin.karima@gmail.com>
2021-02-04 00:23:46 +0300

    save card and build permissions to board extracted

--- app/graphql/mutations/add_card_mutation.rb
+++ app/graphql/mutations/add_card_mutation.rb
@@ -5,26 +5,24 @@ module Mutations
     argument :attributes, Types::CardAttributes, required: true
 
     field :card, Types::CardType, null: true
+
+    # rubocop:disable Metrics/MethodLength
     def resolve(attributes:)
       params = attributes.to_h
       board = Board.find_by!(slug: params.delete(:board_slug))
       card = Card.new(card_params(params, board))
       authorize! card, to: :create?, context: { user: context[:current_user], board: board }
 
-      Card.transaction do
-        card.save
-        Boards::BuildPermissions.new(board, current_user, card: card)
-                                .call(identifiers_scope: 'author')
-        board.save
-      end
+      result = Boards::SaveCardToBoard.new(board, current_user, card).call
 
-      if card.persisted?
+      if result.success?
         RetrospectiveSchema.subscriptions.trigger('card_added', { board_slug: board.slug }, card)
         { card: card }
       else
         { errors: { full_messages: card.errors.full_messages } }
       end
     end
+    # rubocop:enable Metrics/MethodLength
 
     def card_params(params, board)
       params.merge(board: board, author: context[:current_user])
---
+++ app/operations/boards/save_card_to_board.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module Boards
+  class SaveCardToBoard
+    include Dry::Monads[:result]
+    attr_reader :board, :user, :card
+
+    def initialize(board, user, card)
+      @board = board
+      @user = user
+      @card = card
+    end
+
+    def call
+      card.save
+
+      Boards::BuildPermissions.new(board, user, card: card)
+                              .call(identifiers_scope: 'author')
+
+      board.save
+      Success()
+    rescue StandardError => e
+      Failure(e)
+    end
+  end
+end
